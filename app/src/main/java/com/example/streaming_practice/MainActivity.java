85100383decdc44162fa92b80caf2f832ddded0f
haseomg <lovethispool@kakao.com>
2023-03-03 21:57:51 +0900

    will make streaming mp3 file from server

--- app/src/main/java/com/example/streaming_practice/MainActivity.java
+++ app/src/main/java/com/example/streaming_practice/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // 서버의 디렉토리에 있는 mp3 파일을 안드로이드 스튜디오에서 재생시켜야 해
         // 원래 생각한 방식은 mysql 데이터 베이스에 파일을 업로드 시켜서 가져오는 것이였는데,
         // 이 부분에서 너무 막혔어. 다시 정리해서 시간 정해두고 둘 다 구현 시도해보자.
