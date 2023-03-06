0170d1f109a40ed9d7dca6de247b3990406e205e
haseomg <lovethispool@kakao.com>
2023-03-06 20:25:27 +0900

    Android Streaming : mp3 file from Nginx server

--- app/src/main/java/com/example/streaming_practice/MainActivity.java
+++ app/src/main/java/com/example/streaming_practice/MainActivity.java
@@ -2,18 +2,72 @@ package com.example.streaming_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AsyncPlayer;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener {
+
+    String TAG = "[Main Activity]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate();");
 
-        // 서버의 디렉토리에 있는 mp3 파일을 안드로이드 스튜디오에서 재생시켜야 해
-        // 원래 생각한 방식은 mysql 데이터 베이스에 파일을 업로드 시켜서 가져오는 것이였는데,
-        // 이 부분에서 너무 막혔어. 다시 정리해서 시간 정해두고 둘 다 구현 시도해보자.
-        // 토요일 안에 방향 안 잡히면 슬랙에 질문하기
+        // 1. 안드로이드 어플에서 재생 버튼을 클릭한다.
+        // 2. 재생 버튼 클릭 시 SSH 클라이언트를 사용하여 nginx 서버에 접속한다.
+        // 3. mediaplayer 클래스를 사용하여 mp3 파일을 스트리밍한다.
+        // 4. 스트리밍 시 해당 곡의 정보를 서버로부터 받아와서 songInfoTextView에 넣어준다.
+
+        Button playBtn = findViewById(R.id.playButton);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Play Button onClick();");
+
+            }
+        });
+    }
+
+
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
     }
 }
