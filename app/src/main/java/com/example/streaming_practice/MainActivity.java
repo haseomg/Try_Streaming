05586c739c9ac8521b8d0279746542bc8eb8204a
haseomg <lovethispool@kakao.com>
2023-03-06 23:06:19 +0900

    code write complete but is running not yet. because my phone is die... please wake up T T

--- app/src/main/java/com/example/streaming_practice/MainActivity.java
+++ app/src/main/java/com/example/streaming_practice/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.streaming_practice;
 
-import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import android.media.AsyncPlayer;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener {
 
     String TAG = "[Main Activity]";
+
+    private ProgressBar progressBar;
+    private MediaPlayer mediaPlayer;
+    private int mediaFileLengthInMilliseconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +33,14 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnBuf
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate();");
 
-        // 1. 안드로이드 어플에서 재생 버튼을 클릭한다.
-        // 2. 재생 버튼 클릭 시 SSH 클라이언트를 사용하여 nginx 서버에 접속한다.
-        // 3. mediaplayer 클래스를 사용하여 mp3 파일을 스트리밍한다.
-        // 4. 스트리밍 시 해당 곡의 정보를 서버로부터 받아와서 songInfoTextView에 넣어준다.
+        mediaPlayer = new MediaPlayer();
+
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        TextView songInfo = findViewById(R.id.songInfoTextView);
 
         Button playBtn = findViewById(R.id.playButton);
         playBtn.setOnClickListener(new View.OnClickListener() {
@@ -30,9 +48,79 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnBuf
             public void onClick(View v) {
                 Log.i(TAG, "Play Button onClick();");
 
+                playMedia();
+
+                if (playBtn.getText().toString().equals("재생")) {
+                    playBtn.setText("일시 정지");
+                }
+                if (playBtn.getText().toString().equals("재생 중")) {
+                    playBtn.setText("일시 정지");
+                } else {
+                    playBtn.setText("재생 중");
+                }
             }
         });
     }
+
+    private void playMedia() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(("http://13.124.197.8/123.mp3"))
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    mediaPlayer.setDataSource(String.valueOf(response.body().byteStream()));
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepareAsync();
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mediaFileLengthInMilliseconds = mediaPlayer.getDuration();
+                            mediaPlayer.start();
+//                            updateProgressBar();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void updatedProgressBar() {
+        handler.postDelayed(mUpdateTimeTask, 100);
+    }
+
+    private Runnable mUpdateTimeTask = new Runnable() {
+        @Override
+        public void run() {
+            handler.sendEmptyMessage(0);
+            updatedProgressBar();
+        }
+    };
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        progressBar.setSecondaryProgress(percent);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        // 다음 곡 재생 또는 플레이어 종료
+    }
+
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            // 재생 위치 받아오기
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            int progress = (int) (((float) currentPosition / mediaFileLengthInMilliseconds) * 100);
+            progressBar.setProgress(progress);
+        }
+    };
 
 
 
@@ -59,15 +147,12 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnBuf
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy()");
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        } else {
+
+        }
+        mediaPlayer.release();
     }
 
-    @Override
-    public void onBufferingUpdate(MediaPlayer mp, int percent) {
-
-    }
-
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-
-    }
 }
